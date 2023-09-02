f48dd05b09e4837367b7fc246df42295a99aba5c
114576u <114576u@gmail.com>
2023-09-02 12:31:21 +0200

    Correct typo

--- TrackerLibrary/TournamentModel.cs
+++ TrackerLibrary/TournamentModel.cs
@@ -14,7 +14,7 @@ namespace TrackerLibrary
         public int MyProperty { get; set; }
         public List<TeamModel> EnteredTeams { get; set; } = new List<TeamModel>();
         public List<PrizeModel> Prizes { get; set; } = new List<PrizeModel>();
-        public List<List<MatchupModel>> Rounds { get; set; } = new List<List<MatchupModel>>()
+        public List<List<MatchupModel>> Rounds { get; set; } = new List<List<MatchupModel>>();
 
     }
 }
