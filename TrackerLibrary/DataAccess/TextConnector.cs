6513a6cd9c486cdb1680fd63b7eedc707414c262
114576u <114576u@gmail.com>
2023-09-05 16:21:23 +0200

    Completing functionality to save data from Prize form into a csv file

--- TrackerLibrary/DataAccess/TextConnector.cs
+++ TrackerLibrary/DataAccess/TextConnector.cs
@@ -4,17 +4,45 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TrackerLibrary.Models;
+using TrackerLibrary.DataAccess.TextHelpers;
 
 namespace TrackerLibrary.DataAccess
 {
     public class TextConnector : IDataConnection
     {
-        // TODO - Wire up the CreatePrize for text files.
+        
+        private const string PrizesFile = "PrizeModels.csv";
+
         public PrizeModel CreatePrize(PrizeModel model)
         {
-            model.Id = 1;
+            // model.Id = 1;
+            // return model;
+
+            // Load the text file
+            // Convert the text to List<PrizeModel>
+            // Find the ID
+            // Add the new record with the new ID (max + 1)
+            // Convert the prizes to List<string>
+            // Save the List<string> to the text file
+
+            List<PrizeModel> prizes = PrizesFile.FullFilePath().LoadFile().ConvertToPrizeModels();
+
+            // int currentId = prizes.OrderByDescending(x => x.Id).First().Id + 1;
+            int currentId = 1;
+
+            if (prizes.Count > 0)
+            {
+                currentId = prizes.OrderByDescending(x => x.Id).First().Id + 1;
+            }
+
+            model.Id = currentId;
+
+            prizes.Add(model);
+
+            prizes.SaveToPrizeFile(PrizesFile);
 
             return model;
+
         }
     }
 }
